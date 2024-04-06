966900fce043b1526db9531191583011c2c8cc9f
pesspess <spessima@gmail.com>
2024-04-05 22:26:15 -0400

    commit demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
-from pathlib import Path
-import matplotlib.pyplot as plt
+# from pathlib import Path
 import numpy as np
+import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+# import numpy as np
 
 x = np.linspace(0, 20, 100) # create a list of evenly-space numbers over the range
 plt.plot(x, np.sin(x))      # plot the sine of each x point
