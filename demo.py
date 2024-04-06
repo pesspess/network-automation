ff39b17cec0985147260ea895b655a20aca0889f
pesspess <spessima@gmail.com>
2024-04-05 22:46:50 -0400

    commit changes

--- demo.py
+++ demo.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 # import matplotlib.pyplot as plt
 # import numpy as np
-import General.Plotting.auxiliary_plotting_functions as aux_plot  # pylint: disable=import-error
+#import General.Plotting.auxiliary_plotting_functions as aux_plot  
+# pylint: disable=import-error
 
 x = np.linspace(0, 20, 100) # create a list of evenly-space numbers over the range
 plt.plot(x, np.sin(x))      # plot the sine of each x point
